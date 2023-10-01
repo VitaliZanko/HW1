b222a14b711a430dc194fdd00e88813c2166fb96
Vitali Zanko <videxy95@gmail.com>
2023-10-01 23:16:40 +0300

    This is my HW

---
+++ task2/Program.cs
@@ -0,0 +1,20 @@
+﻿// Task1
+int max = 0, min = 0;
+
+Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if(a > b)
+{
+    max = a;
+    min = b;
+}
+    else
+    {
+        max = b;
+        min = a;
+    }
+Console.WriteLine("max = " + max + " и " + "min = " + min);
